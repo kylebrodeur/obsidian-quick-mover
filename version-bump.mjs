440519d392cc7326cdff0a4209539fd91bf67052
kylebrodeur <kyle@brodeur.me>
2025-02-08 10:57:25 -0600

    fix: versioning

--- version-bump.mjs
+++ version-bump.mjs
@@ -3,13 +3,21 @@ import process from 'process';
 
 const targetVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+// Update manifest.json
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+// Update versions.json
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+// Sort versions in descending order
+const sortedVersions = Object.fromEntries(
+    Object.entries(versions).sort(([a], [b]) => {
+        return -1 * a.localeCompare(b, undefined, { numeric: true });
+    })
+);
+
+writeFileSync("versions.json", JSON.stringify(sortedVersions, null, 4) + "\n");
